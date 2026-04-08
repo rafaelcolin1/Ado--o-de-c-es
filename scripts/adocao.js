cf5f4abdb413db5c721d21e6d04deed7591d3e13
rafael_colin <rafael_colin@estudante.sesisenai.org.br>
2026-04-08 12:50:33 -0300

    add - save

--- scripts/adocao.js
+++ scripts/adocao.js
@@ -14,36 +14,31 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
 
     let pet = document.querySelector('input[name="pet"]:checked');
 
-    console.log(pet.value);
+    if (nome.length < 3) return alert("Nome Inválido");
 
+    if (cpf.length == 0) return alert("CPF Inválido");
 
-
+    if (telefone.length < 8) return alert("Telefone Inválido");
 
     if (idade < 18) {
 
         alert("Menor de Idade Não Pode Adotar");
     }
 
+    if (cidade.length == 0) return alert("Cidade Inválido");
+
     if (animal > 8) {
         alert("Você não pode adotar esse animal, pois ficará tempo de mais sozinho");
     }
 
-    if (nome.length < 3) return alert("Nome Inválido");
 
     if (adocao.length < 10) return alert("Motivo Inválido");
 
-    if (telefone.length < 8) return alert("Telefone Inválido");
+    if (quintal.value == "") return alert("Campo obrigatório");
 
     if (pet.value == "petnao") return alert("O animal poderá haver acompanhamento da ONG");
 
     if (pet.value == "") return alert("Campo obrigatório");
-
-    if (quintal.value == "") return alert("Campo obrigatório");
-
-    if (cpf.length == 0) return alert("CPF Inválido");
-
-    if (cidade.length == 0) return alert("Nome Inválido");
-
 
 
     document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
