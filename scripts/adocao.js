bf06796a2bffd987103b415711fdf696c4616604
rafael_colin <rafael_colin@estudante.sesisenai.org.br>
2026-04-08 12:27:47 -0300

    add - save

--- scripts/adocao.js
+++ scripts/adocao.js
@@ -1,21 +1,50 @@
 document.getElementById("formAdocao").addEventListener("submit", function (e) {
-e.preventDefault();
+    e.preventDefault();
 
-let nome = document.getElementById("nome").value;
-let email = document.getElementById("email").value;
-let moradia = document.getElementById("moradia").value;
-let quintal = document.querySelector('input[name="quintal"]:checked');
-let cpf = document.getElementById("cpf").value;
-let idade = document.getElementById("idade").value;
-function idademin(idade){
-    const min = 18;
-    if (parseFloat(idade.value) < min) {
-        idade.value = min;
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let moradia = document.getElementById("moradia").value;
+    let quintal = document.querySelector('input[name="quintal"]:checked');
+    let cpf = document.getElementById("cpf").value;
+    let telefone = document.getElementById("telefone").value;
+    let idade = document.getElementById("idade").value;
+    let cidade = document.getElementById("cidade").value;
+    let animal = document.getElementById("animal").value;
+    let adocao = document.getElementById("adocao").value;
+
+    let pet = document.querySelector('input[name="pet"]:checked');
+
+    console.log(pet.value);
+
+
+
+
+    if (idade < 18) {
+
+        alert("Menor de Idade Não Pode Adotar");
     }
-}
-let cidade = document.getElementById("cidade").value;
-let animal = document.getElementById("animal").value;
-let adocao = document.getElementById("adocao").value;
 
-document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " +nome;
+    if (animal > 8) {
+        alert("Você não pode adotar esse animal, pois ficará tempo de mais sozinho");
+    }
+
+    if (nome.length < 3) return alert("Nome Inválido");
+
+    if (adocao.length < 10) return alert("Motivo Inválido");
+
+    if (telefone.length < 8) return alert("Telefone Inválido");
+
+    if (pet.value == "petnao") return alert("O animal poderá haver acompanhamento da ONG");
+
+    if (pet.value == "") return alert("Campo obrigatório");
+
+    if (quintal.value == "") return alert("Campo obrigatório");
+
+    if (cpf.length == 0) return alert("CPF Inválido");
+
+    if (cidade.length == 0) return alert("Nome Inválido");
+
+
+
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
 });
