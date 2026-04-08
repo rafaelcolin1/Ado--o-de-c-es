8c4cf0b55724ab9bf5b01fe3df984937e4fdcd92
rafael_colin <rafael_colin@estudante.sesisenai.org.br>
2026-04-08 10:14:21 -0300

    add - save

--- scripts/adocao.js
+++ scripts/adocao.js
@@ -2,12 +2,20 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
 e.preventDefault();
 
 let nome = document.getElementById("nome").value;
-let email document.getElementById("email").value;
+let email = document.getElementById("email").value;
 let moradia = document.getElementById("moradia").value;
 let quintal = document.querySelector('input[name="quintal"]:checked');
+let cpf = document.getElementById("cpf").value;
+let idade = document.getElementById("idade").value;
+function idademin(idade){
+    const min = 18;
+    if (parseFloat(idade.value) < min) {
+        idade.value = min;
+    }
+}
+let cidade = document.getElementById("cidade").value;
+let animal = document.getElementById("animal").value;
+let adocao = document.getElementById("adocao").value;
 
-if(nome.length < 3) return alert("Nome Inválido");
-
-document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " +
-nome;
+document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " +nome;
 });
